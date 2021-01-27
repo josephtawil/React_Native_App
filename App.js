9eb5ce18388b32081d960f4e33a33b3309fa74f5
Joseph <jtawil2015@gmail.com>
2021-01-27 14:15:42 -0800

    added input box and displayes todos

--- App.js
+++ App.js
@@ -1,15 +1,47 @@
-import React,{Component} from 'react';
-import {View, SafeAreaView} from 'react-native';
-
+import React,{Component, useState} from 'react';
+import {StyleSheet, ScrollView,  TextInput, Text, View, SafeAreaView, Button} from 'react-native';
+import Todo from './components/Todo';
+//use uuid for keys
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    setTodos([input, ...todos]);
+    setInput('');
+  }
   return (
     <SafeAreaView>
-      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
-      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}}/>
-      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}}/>
+      <View>
+          <Text style={styles.firstLabel}>Let's Build a React Native App in One Day</Text>
+      </View>
+
+     <ScrollView>
+     {todos.map(todo=>(
+        <Todo title={todo}/>
+      ))}
+     </ScrollView>
+     
+      <TextInput style = { styles.todoInput} value={input} onChangeText={text => setInput(text)}/>
+      <Button title="Add Button" onPress={addTodo}/>
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+  buttonStyle:{
+    borderWidth: 20
+  },
+  todoInput: {
+    margin: 20,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  firstLabel: {
+    // backgroundColor: 'red',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
 export default App
