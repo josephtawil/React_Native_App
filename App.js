010a134e53a1ff027c61feceb81943c811e33659
Joseph <jtawil2015@gmail.com>
2021-02-01 20:24:18 -0800

    added graph

--- App.js
+++ App.js
@@ -1,40 +1,106 @@
-import React,{Component, useState} from 'react';
+import React,{Component, useState, useEffect} from 'react';
 import {StyleSheet, ScrollView,  TextInput, Text, View, SafeAreaView, Button} from 'react-native';
+import Graph from './components/Graph'
+import moment from 'moment';
 import Todo from './components/Todo';
 //use uuid for keys
 
 const App = () => {
   const [description, setDescription] = useState('');
   const [amount, setAmount] = useState('');
-  const [gisg, setGigs] = useState([
+  const [labels, setLabels] = useState([]);
+  const [dataPoints, setDataPoints] = useState([
     {
-      description: 'Freelance Job',
-      amount: 400.00
+      [moment()]: 2000
+    },
+    {
+      [moment().subtract(1,'days')]: 2000
+    },
+    {
+      [moment().subtract(2, 'days')]: 3000
+    },
+    {
+      [moment().subtract(3, 'days')]: 4000
+    },
+    {
+      [moment().subtract(4, 'days')]: 5000
     }
   ]);
+  
+  const [gigs, setGigs] = useState([
+    {
+      description: 'Freelance Job',
+      amount: 400.00,
+      timestamp: new Date()
+    },
+    {
+      description: 'Freelancer',
+      amount: 700.00,
+      timestamp: new Date()
+    }
+  ]);
+  const [totalAmount, setTotalAmount] = useState(0);
 
+  useEffect(() => {
+    
+  }, [gigs])
+
+
+  useEffect(() =>{
+    const total = gigs.reduce((total, gig) =>{
+      return total + Number(gig.amount)
+    }, 0);
+
+    setTotalAmount(total);
+  }, [gigs]);
+
+  const addGig =  () =>{
+    setGigs([...gigs, { 
+      description: description,
+      amount: amount
+    }]);
+
+    setDescription('');
+    setAmount('');
+  }
   return (
     <SafeAreaView>
       <View>
           <Text style={styles.firstLabel}> 🍏 Income Tracker</Text>
       </View>
 
-    
-      <TextInput placeholder = "Enter description" keyboardType = "default"  style = { styles.todoInput} value={description} onChangeText={text => setDescription(text)}/>
+      <View>
+        <Text> Total Income ${totalAmount}</Text>
+      </View>
+      <TextInput placeholder = "Enter description" keyboardType = "default"  style = { styles.input} value={description} onChangeText={text => setDescription(text)}/>
 
-      <TextInput placeholder = "Enter the amount you made" keyboardType = "numeric" style = { styles.todoInput} value={amount} onChangeText={text => setAmount(text)}/>
-      {/* <Button title="Add Button" onPress={addTodo}/> */}
+      <TextInput placeholder = "Enter the amount you made in USD 💰 " keyboardType = "numeric" style = { styles.input} value={amount} onChangeText={text => setAmount(text)}/>
+      <Button disabled = {!amount && !description} title="Add Gig 🚀 " onPress={addGig}/>
+
+      {gigs.map(gig=>(
+        <View>
+          <Text>
+            {gig.description}
+          </Text>
+          <Text>
+           ${gig.amount}
+          </Text>
+          <Text>
+
+          </Text>
+        </View>
+      ))}
+
+      <Graph data={gigs}/>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  buttonStyle:{
-    borderWidth: 20
-  },
-  todoInput: {
+  input: {
     margin: 20,
     height: 40,
+    padding: 10,
     borderColor: 'black',
     borderWidth: 1
   },
---
+++ components/Graph.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {StyleSheet, Dimensions,ScrollView,  TextInput, Text, View, SafeAreaView, Button} from 'react-native';
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
+const Graph = ({data}) => {
+    return (
+        <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"],
+            datasets: [
+              {
+                data: [
+                 data[0].amount,
+                 data[1].amount
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width} // from react-native
+          height={220}
+          yAxisLabel="$"
+        //   yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "green",
+            backgroundGradientTo: "blue",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726"
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16
+          }}
+        />
+      </View>
+    )
+}
+
+export default Graph
